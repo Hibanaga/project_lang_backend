e889c2a68be82d7571d48edf09fa548318974ec3
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2022-02-19 13:37:04 +0100

    modify services

--- src/config/default.ts
+++ src/config/default.ts
@@ -4,6 +4,6 @@ export const config: IConfigProps = {
   uri:
     process.env.MONGO_URI ||
     `mongodb+srv://hibana_main:Zaxscd1212@clusterprojectlang.jt1vg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-  creator_mail: process.env.CREATOR_MAIL || 'coollearn.info@gmail.com',
-  creator_pass: process.env.CREATOR_PASS || 'Zaxscd1212',
+  creator_mail: 'coollearn.info@gmail.com',
+  creator_pass: 'Zaxscd1313',
 };
